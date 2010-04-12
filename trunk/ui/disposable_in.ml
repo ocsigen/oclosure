c9b97a91fd29cd153cea490bf5d2b8ac9f6b7ab6
cardoso <none>
2010-04-12 14:13:21 +0200

    Ignore-this: 1cef10fabfd6a1ae8e419d0eba6433bb Module Disposable
    darcs-hash:20100412121321-e782f-73e6c668183f710e489ada48ca7afa816fdc6dd9

---
+++ trunk/ui/disposable_in.ml
@@ -0,0 +1,15 @@
+(* 
+ * Module goog.Disposable
+ * Class that provides the basic implementation for disposable objects. 
+ * If your class holds one or more references to COM objects, DOM nodes, or 
+ * other disposable objects, it should extend this class or implement the 
+ * disposable interface.
+ * Cardoso Gabriel - 2010
+ *)
+
+class external disposable : 
+  < 
+  dispose: unit -> unit;
+ getDisposed: unit -> bool;
+ isDisposed: unit -> bool;
+ >
