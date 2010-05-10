09c9d8962f77460c0f2e0356e7533ebbfcd80d50
eelsther <none>
2010-05-10 17:45:45 +0200

    Ignore-this: 7383ffc546edb91e80bad4a43cb62c28
    darcs-hash:20100510154545-b7674-932a5755e0af561965a203c83350c8c53ccb0f11

--- trunk/goog/positioning/abstractPosition_in.ml
+++ trunk/goog/positioning/abstractPosition_in.ml
@@ -7,7 +7,19 @@
 (** JSOO.obj **)
 type element = JSOO.obj
 (** goog.positioning.Corner **)
-type corner 
+type corner = 
+(*goog.positioning.Corner = {
+  TOP_LEFT: 0,
+  TOP_RIGHT: 2,
+  BOTTOM_LEFT: 1,
+  BOTTOM_RIGHT: 3,
+  TOP_START: 4,
+  TOP_END: 6,
+  BOTTOM_START: 5,
+  BOTTOM_END: 7
+};
+*)
+
 (** goog.math.Box **)
 type box
 (** goog.math.Size **)
@@ -16,5 +28,5 @@ type size
 class external abstractPosition :
   < 
   (**  Repositions the element. Abstract method, should be overloaded **)
-  reposition: element -> corner -> box -> size -> unit
-  >
+  reposition: element -> corner -> (*opt args box -> size ->*) unit
+  > = "goog.positioning.AbstractPosition"
--- trunk/test/test_popup.ml
+++ trunk/test/test_popup.ml
@@ -1,7 +1,10 @@
-open Popup
+(* Test Popup 
+ * Esther Baruk - 2010 *)
 
+open Popup
+open AnchoredPosition
 
 let p = Js.get_element_by_id "popup"
 let popup_elt = new popup p
-
+let _ = popup_elt # setPosition (new anchoredPosition p 1)
 let _ = popup_elt # setVisible true
