eb2fff1ae8cbf4bff72e1996893b4187fd102b0a
charles <none>
2010-05-25 21:11:45 +0200

    Ignore-this: b9245435e717f4157c830df33fbe2bf9 Ajout de autoComplete, du test de Charcounter et modifications de zippy
    darcs-hash:20100525191145-cbdb0-5abd4aab96f2515738fd790e3bbdfceafdceba5a

--- trunk/test/test_charcounter.ml
+++ trunk/test/test_charcounter.ml
@@ -15,21 +15,21 @@ open Html
 
 let _ =
   let input1 = Js.get_element_by_id "input1"
-  and counter1 = Js.get_element_by_id "counter1" in 
-    ignore(new Charcounter.charcounter input1 counter1 160. 0);
+  and counter1 = Js.get_element_by_id "counter1" in
+    ignore(new Charcounter.charcounter input1 counter1 160. Charcounter.display_Remaining);
 
   let input2 = Js.get_element_by_id "input2"
   and counter2 = Js.get_element_by_id "counter2" in
-    ignore(new Charcounter.charcounter input2 counter2 160. 1);
+    ignore(new Charcounter.charcounter input2 counter2 160. Charcounter.display_Incremental);
 
   let input3 = Js.get_element_by_id "input3"
   and counter3 = Js.get_element_by_id "counter3"
   and set10 = Js.get_element_by_id "set1"
   and set20 = Js.get_element_by_id "set2" in
-  let test = new Charcounter.charcounter input3 counter3 10. 0 in
+  let test = new Charcounter.charcounter input3 counter3 10. Charcounter.display_Remaining in
     register_event_opt set10 "onclick" (Some (fun () -> test#setMaxLength 10.));
     register_event_opt set20 "onclick" (Some(fun () -> test#setMaxLength 20.));
 
   let input4 = Js.get_element_by_id "input4" in
   let counter4 = Js.get_element_by_id "counter4" in
-    ignore(new Charcounter.charcounter input4 counter4 255. 0);
+    ignore(new Charcounter.charcounter input4 counter4 255. Charcounter.display_Remaining);
--- trunk/test/test_zippy.ml
+++ trunk/test/test_zippy.ml
@@ -9,15 +9,9 @@
    * @version 0.1
 *)
 
-let f a = 
-  Js.alert a
 
 let _ =  
   let z1 = new Zippy.zippy "header1" "content1" in
     z1#expand();
-  let z2 = new AnimatedZippy.animatedzippy "header2" "content2" false in
-    z2#isBusy();
-  let z3 = new AnimatedZippy.animatedzippy "header3" "content3" true in
-    z3#isBusy() 
-  (*let event = new Eventsobj.eventObj () in
-    event#listen (Js.get_element_by_id "header1") "toggle" f*)
+    ignore(new AnimatedZippy.animatedzippy "header2" "content2" false);
+    new AnimatedZippy.animatedzippy "header3" "content3" true
