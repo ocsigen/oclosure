0f4ed7334ff1445076dcc0521dc5453d7a66f8d9
charles <none>
2010-06-15 16:41:36 +0200

    Ignore-this: 459d8df9d7cfd7ead7946159023de3f6 Ajout de l'exemple de l'autoCompletion basique
    darcs-hash:20100615144136-cbdb0-c278ec5a915303c77da973c183404ab74d1e777b

---
+++ trunk/examples/test_autoCompleteBasic.ml
@@ -0,0 +1,26 @@
+(**
+   * OClosure Project - 2010
+   * Class goog.ui.AutoComplete
+   *
+   * This is the central manager class for an AutoComplete instance.
+   * 
+   * @author : Oran Charles
+   * @version 0.2
+   * @see 'goog.events.EventTarget'
+   * @see 'goog.events.Event'
+*)
+module D = Dom_html
+let d = D.document
+
+let _ =
+  let array = [|"OCaml";"O'Browser";"Mon Colonel";"O'Closure";
+	      "Caml";"Ocsigen";"Javascript";"Binding";"Closure";
+	      "Pikachu";"Haskell";|] 
+  in
+  let div1 = Js.Opt.get (d##getElementById (Js.string "input1"))
+    (fun () -> assert false)
+  and div2 = Js.Opt.get (d##getElementById (Js.string "input2"))
+    (fun () -> assert false)
+  in
+    ignore(jsnew Ui.AutoCompleteBasic.basic (Js.array array,div1,Js.some (Js.bool false),Js.null));
+    jsnew Ui.AutoCompleteBasic.basic (Js.array array,div2,Js.some (Js.bool true),Js.null)
