8fc4d42de569f0bef3d699ee65bdbca911204c56
gregoire.henry <gregoire.henry@pps.jussieu.fr>
2011-06-30 18:03:23 +0200

    Use google dependencies calculator to minimize the time needed to load the library
    Ignore-this: 244d5b99a5487b166c9d22c15fb55f
    
    See: http://code.google.com/closure/library/docs/closurebuilder.html
    
    darcs-hash:20110630160323-da4f2-252e0155b912c58f7955cc2aa8ee82b5efad128a

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+
+include Makefile.config
+
 LIBRARY=oclosure
 DIRS=goog examples
 
-all: lib
+all: lib oclosure_req
 
 .PHONY: lib examples doc
 
@@ -9,15 +12,29 @@ lib:
 	@echo "[MAKE] in goog"
 	@(${MAKE} -C goog);
 
+oclosure_req: scripts/requirements.ml
+	@echo "[OCAMLC] oclosure_req"
+	@ocamlfind ocamlc -o $@ -package str,findlib,unix -linkpkg $^
+
 examples: lib
 	@echo "[MAKE] in examples"
 	@(${MAKE} -C examples);
 
+DESTDIR:=$(shell ocamlfind printconf destdir)
+
 install:
-	ocamlfind install $(LIBRARY) goog/goog.cmi scripts/requirements.ml goog/oclosure.cma META
+	ocamlfind install $(LIBRARY) goog/goog.cmi goog/oclosure.cma META
+	cp -r google-closure/closure google-closure/third_party $(DESTDIR)/$(LIBRARY)
+	chmod +x $(DESTDIR)/$(LIBRARY)/closure/bin/build/closurebuilder.py
+	install -D -m 755 oclosure_req ${BINDIR}/oclosure_req
 
 uninstall:
-	ocamlfind remove $(LIBRARY)
+	-[ ! -d $(DESTDIR)/$(LIBRARY)/closure ] || rm -rf $(DESTDIR)/$(LIBRARY)/closure
+	-[ ! -d $(DESTDIR)/$(LIBRARY)/third_party ] || rm -rf $(DESTDIR)/$(LIBRARY)/third_party
+	-[ ! -f ${BINDIR}/oclosure_req ] || rm -f ${BINDIR}/oclosure_req
+	-ocamlfind remove $(LIBRARY)
+
+reinstall: uninstall install
 
 doc:
 	@(${MAKE} -C goog doc);
--- scripts/requirements.ml
+++ scripts/requirements.ml
@@ -1,24 +1,98 @@
-let req = Str.regexp "\\[oclosure\\]\\([^[]*\\)\\[/oclosure\\]"
 
-let rec search l p o = 
-  let p = Str.search_forward req l p
-  in Printf.fprintf o "goog.require(\"%s\");\n" (Str.matched_group 1 l);
-  search l (p+1) o
+let verbose = ref false
+let compile = ref true
 
-let search_requirements i o =
-  begin try
-    while true do 
-      let l = input_line i in
-      try search l 0 o with Not_found -> ()
-    done
-  with End_of_file -> ()
-  end
+let package_name = "oclosure"
+
+let closure_dir =
+  Findlib.init ();
+  (* FIXME package name from Makefile ${LIBRARY} *)
+  Filename.concat (Findlib.package_directory package_name) "closure"
+
+let third_party_dir =
+  Filename.concat (Findlib.package_directory package_name) "third_party"
+
+let roots = [ closure_dir; third_party_dir]
+
+let closure_builder_path =
+  Filename.concat
+    (Filename.concat
+       (Filename.concat
+	  closure_dir
+	  "bin")
+       "build")
+    "closurebuilder.py"
+
+let compiler_jar =
+  ref (Filename.concat (Findlib.package_directory package_name) "compiler.jar")
+
+let argument = [
+  "-verbose", Arg.Set verbose, "Be verbose";
+  "-script", Arg.Clear compile, "Do not optimize the generated javascript";
+  "-compiler_jar", Arg.Set_string compiler_jar, "Path to the javascript compiler";
+]
+
+let usage = Printf.sprintf "%s [options] eliom_appl.js ..." Sys.argv.(0)
+
+let req_re = Str.regexp "\\[oclosure\\]\\([^[]*\\)\\[/oclosure\\]"
+
+let rec search_line line pos =
+  try
+    let pos = Str.search_forward req_re line pos in
+    let req = Str.matched_group 1 line in
+    req :: search_line line (pos+1)
+  with Not_found -> []
+
+let rec search_channel in_ch =
+  try
+    let line = input_line in_ch in
+    search_line line 0 @ search_channel in_ch
+  with End_of_file -> []
+
+let search_requirements file =
+  let in_ch = open_in file in
+  let reqs = search_channel in_ch in
+  close_in in_ch;
+  reqs
+
+let eliom_js_file = Sys.argv.(1)
 
 
-let file = Sys.argv.(1)
+let rec print_roots () = function
+  | [] -> ""
+  | root :: roots ->
+      Printf.sprintf " --root=%s%a" root print_roots roots
 
-let i = open_in file
-let o = open_out (Filename.chop_suffix file ".js"^"_req.js")
-let _ = search_requirements i o
+let rec print_reqs () = function
+  | [] -> ""
+  | req :: reqs ->
+      Printf.sprintf " --namespace=%s%a" req print_reqs reqs
 
-let _ = close_out o
+let file_exists file = try ignore(Unix.stat file); true with _ -> false
+
+let process_file file =
+  if not (Filename.check_suffix file "js") then begin
+    prerr_endline usage;
+    exit 1
+  end;
+  let requirements = search_requirements file in
+  let output_file = Filename.chop_extension file ^ "_oclosure.js" in
+  let command =
+    if !compile && file_exists !compiler_jar then
+      Printf.sprintf "%s%a%a --compiler_jar=%s -o compiled --output_file=%s"
+	closure_builder_path
+	print_roots roots
+	print_reqs requirements
+	!compiler_jar
+	output_file
+    else
+      Printf.sprintf "%s%a%a -o script --output_file=%s"
+	closure_builder_path
+	print_roots roots
+	print_reqs requirements
+	output_file in
+  if !verbose then print_endline ("+ " ^ command);
+  let res =  Sys.command command in
+  if res <> 0 then exit res
+
+let _ = Arg.parse argument process_file usage
